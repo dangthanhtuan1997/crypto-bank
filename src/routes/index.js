6ea5a161968bf4b8c3fd57761cd4e445f1c64dee
Đặng Thanh Tuấn <contact.dangthanhtuan@gmail.com>
2020-05-23 09:36:54 +0700

    fix heroku crash

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('./auth.route');
-const user = require('./user.route');
+const partner = require('./partner.route');
 const transaction = require('./transaction.route');
 
 module.exports = () => {
     //auth(router);
-    user(router);
+    partner(router);
     transaction(router);
     
     return router;
--- src/routes/partner.route.js
+++ src/routes/partner.route.js
@@ -9,9 +9,9 @@ generateAccountNumber = (length) => {
 }
 
 module.exports = (app) => {
-    app.use('/users', router);
+    app.use('/partners', router);
 
-    router.get('/:account_number', verifyPartner, async (req, res) => {
+    router.get('/users/:account_number', verifyPartner, async (req, res) => {
         const user = await User.findOne({ account_number: req.params.account_number });
         if (!user){
             return res.status(404).json({message: 'not found'});
@@ -21,10 +21,10 @@ module.exports = (app) => {
         return res.status(200).json(userModified);
     });
 
-    router.post('/', verifyPartner, async (req, res) => {
-        const ac = generateAccountNumber(13);
-        const user = new User({ ...req.body, account_number: ac });
-        await user.save();
-        return res.status(201).json(user);
-    });
+    // router.post('/', verifyPartner, async (req, res) => {
+    //     const ac = generateAccountNumber(13);
+    //     const user = new User({ ...req.body, account_number: ac });
+    //     await user.save();
+    //     return res.status(201).json(user);
+    // });
 };
